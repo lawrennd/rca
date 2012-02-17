78e338183365abde7b6bea8c7bfab1fdef7f3363
alkalait <alkalait@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2012-02-17 18:27:10 +0000

    results on combined-walk-dance motions (w/o induced noise)

--- matlab/demCMUMocapEMRca2.m
+++ matlab/demCMUMocapEMRca2.m
@@ -25,11 +25,8 @@ figure(1), clf, colormap('hot'); figure(2), clf, colormap('hot'); figure(3), clf
 d = 31;
 Lambda = skelConnectionMatrix(acclaimReadSkel('01.asf'));   Lambda = Lambda + Lambda' + eye(d);
 nonZero = find(ones(d));                    % Induce any prior knowledge of zeros. If none, this is all ones.
-lambda = 5.^linspace(-8,10,30);
-% lambda = 5.^linspace(1,5,30);
-% lambda = 5.^linspace(4,10,10);
+lambda = 5.^linspace(-8,3,30);
 Lambda_hat = cell(length(lambda),1);
-emrca_options = struct('limit',1e-4, 'showProgress',0 , 'verbose',0, 'errorCheck',1, 'maxNumIter',250);
 options = struct('verbose',1,'order',-1);
 GLASSOrocstats = zeros(length(lambda), 4);   EMRCArocstats = zeros(length(lambda), 4);
 figure(1), imagesc(Lambda), colorbar, title('Ground truth network')
@@ -43,7 +40,7 @@ load walk_dance_Channels.mat
 %% Compute centred squared-distance matrix (kernel) for every frame.
 H = eye(d) - ones(d)./d;
 counter = 0;
-Y = {zeros(100000,31) zeros(100000,31)};
+Ytemp = {zeros(100000,31) zeros(100000,31)};
 Yaux = zeros(100000,31);
 xyz = {zeros(10000,93) zeros(10000,93)};
 Channels = {walkChannels danceChannels}; XYZChannels = {walkXYZChannels danceXYZChannels}; Skeletons = {walkSkeletons danceSkeletons};
@@ -60,14 +57,15 @@ for iCh = 1:length(Channels)
         end
     end
     isIll = true;
-    Y{iCh} = zeros(nFrames(iCh)*3,d);
+    Ytemp{iCh} = zeros(nFrames(iCh)*3,d);
     while isIll
-        nicexyz = xyz{iCh}(randsample(nFrames(iCh),100),:);
+        nicexyz = xyz{iCh}(randsample(nFrames(iCh),min(3000,nFrames(iCh))),:);
         for i = 1:size(nicexyz,1)
             X = reshape(nicexyz(i,:), [], 3);
-            Y{iCh}((i-1)*3+1:i*3,:) = (H*X)';
+            X = X + randn(size(X))*2;
+            Ytemp{iCh}((i-1)*3+1:i*3,:) = (H*X)';
         end
-        [B, U, jitter] = pdinv(Y{iCh}'*Y{iCh} / size(Y{iCh},1));
+        [B, U, jitter] = pdinv(Ytemp{iCh}'*Ytemp{iCh} / size(Ytemp{iCh},1));
         rocstats(iCh,:) = emrcaRocStats(Lambda, B<0);   % Measure of how good B is for initializing Lambda_hat.
         if cond(B) < 1e+10
             isIll = false;
@@ -99,7 +97,7 @@ end
 %     end
 % end
 %}
-Y = Y{2};   % 1 for walking, 2 for dancing
+Y = [Ytemp{1}; Ytemp{2}];   % 1 for walking, 2 for dancing
 Cy = Y'*Y / size(Y,1);
 figure(2), imagesc(Cy), colorbar, title('Sum of centred sq.distance matrices across frames')
 figure(5), imagesc(pdinv(Cy)), colorbar, title('empirical inverse-covariance ')
@@ -121,17 +119,19 @@ Recalls = TPs ./ (TPs + FNs);   Precisions = TPs ./ (TPs + FPs);
 jit = randn(length(lambda),1)*.001;
 figure(3), hold on, plot(Recalls, Precisions, '-xb', 'Markersize',10), xlim([0 1]), ylim([0 1]), xlabel('Recall'), ylabel('Precision')
 figure(3), hold on, plot(rocstats(:,1) ./ (rocstats(:,1) + rocstats(:,3)), rocstats(:,1) ./ (rocstats(:,1) + rocstats(:,2)), 'sr'), legend('Glasso','inv.cov')
-% text(Recalls+jit, Precisions+jit, num2cell(lambda),'fontsize',8)
+text(Recalls+jit, Precisions+jit, num2cell(lambda))
 
 
 %% Recovery of sparse-inverse and low-rank covariance via iterative application of EM and RCA.
+lambda = 5.^linspace(-8,-4,30);
+emrca_options = struct('limit',1e-4, 'showProgress',0 , 'verbose',0, 'errorCheck',1, 'maxNumIter',250);
 sigma2_n = .01*trace(Cy);                                   % Noise variance.
 [S D] = eig(Cy);     [D perm] = sort(diag(D),'descend');    % Initialise W with a PCA low-rank estimate.
 W_hat_old = S(:,perm(D>sigma2_n)) * sqrt(diag(D(D>sigma2_n)-sigma2_n));
 WWt_hat_old = W_hat_old * W_hat_old';
 Lambda_hat_new = cell(length(lambda),1);
 Lambda_hat_old = eye(d); % pdinv(Cy); %                                % Initialise Lambda_hat with the empirical inverse-covariance.
-for (i = 1:length(lambda))                             % Try different magnitudes of lambda.
+parfor (i = 1:length(lambda),8)                             % Try different magnitudes of lambda.
     [WWt_hat_new, Lambda_hat_new{i}, Lambda_hat_new_inv] = emrca(Y, WWt_hat_old, Lambda_hat_old, sigma2_n, lambda(i), nonZero, emrca_options);
     % Plot results.
     figure(5), clf, subplot(121), imagesc(Lambda_hat_new{i}), colorbar, title([ 'EM/RCA-recovered \Lambda with \lambda=', num2str(lambda(i)) ]);
@@ -143,5 +143,5 @@ end
 TPs = EMRCArocstats(:,1); FPs = EMRCArocstats(:,2); FNs = EMRCArocstats(:,3); TNs = EMRCArocstats(:,4);
 Recalls = TPs ./ (TPs + FNs);   Precisions = TPs ./ (TPs + FPs);
 FPRs = FPs ./ (FPs + TNs);      AUC = trapz(flipud(FPRs), flipud(Recalls)) / max(FPRs);
-figure(3), hold on, plot(Recalls+jit, Precisions+jit, '-og'), xlim([0 1]), ylim([0 1]), xlabel('Recall'), ylabel('Precision'), legend('inv.cov','Glasso','EM-RCA')
-% text(Recalls+jit, Precisions+jit, num2cell(lambda))
+figure(3), hold on, plot(Recalls, Precisions, '-og'), xlim([0 1]), ylim([0 1]), xlabel('Recall'), ylabel('Precision'), legend('Glasso','inv.cov','EM-RCA')
+text(Recalls+jit, Precisions+jit, num2cell(lambda))
